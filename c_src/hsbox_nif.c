83cd897ceebd5ce20853e50dffe168b598aacd0c
Stefan Grundmann <sg2342@googlemail.com>
2024-03-18 17:38:33 +0000

    poly1305 cbits from https://github.com/kazu-yamamoto/crypton

--- c_src/hsbox_nif.c
+++ c_src/hsbox_nif.c
@@ -1,25 +1,14 @@
 #include "erl_nif.h"
 #include "crypton_xsalsa.h"
+#include "crypton_poly1305.h"
 
 
-static ErlNifResourceType *salsa_ctx;
-
-static ERL_NIF_TERM inc_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
-  int y, ret;
-  if (!enif_get_int(env, argv[0], &y)) {
-    return enif_make_badarg(env);
-  }
-  ret = y + 1;
-  return enif_make_int(env, ret);
-}
+static ErlNifResourceType *r_salsa_ctx;
+static ErlNifResourceType *r_poly1305_ctx;
 
 static ERL_NIF_TERM xsalsa20_init_nif(ErlNifEnv* env, int argc,
 				      const ERL_NIF_TERM argv[])
 {
-  void *ctx = enif_alloc_resource(salsa_ctx, sizeof(crypton_salsa_context));
-  ERL_NIF_TERM ret = enif_make_resource(env, ctx);
-
   uint32_t nb_rounds;
   ErlNifBinary key, nonce;
 
@@ -28,10 +17,13 @@ static ERL_NIF_TERM xsalsa20_init_nif(ErlNifEnv* env, int argc,
       !enif_inspect_binary(env, argv[2], &nonce))
     return enif_make_badarg(env);
 
+  void *ctx = enif_alloc_resource(r_salsa_ctx, sizeof(crypton_salsa_context));
+  ERL_NIF_TERM ret = enif_make_resource(env, ctx);
+
   crypton_xsalsa_init((crypton_salsa_context*) ctx, nb_rounds,
 		      key.size, key.data, nonce.size, nonce.data);
 
-  return enif_make_tuple2(env, enif_make_atom(env, "ok"), ret);  
+  return ret;
 }
 
 static ERL_NIF_TERM xsalsa20_derive_nif(ErlNifEnv* env, int argc,
@@ -40,11 +32,12 @@ static ERL_NIF_TERM xsalsa20_derive_nif(ErlNifEnv* env, int argc,
   void *ctx;
   ErlNifBinary nonce;
 
-  if (!enif_get_resource(env, argv[0], salsa_ctx, &ctx) ||
+  if (!enif_get_resource(env, argv[0], r_salsa_ctx, &ctx) ||
       !enif_inspect_binary(env, argv[1], &nonce))
     return enif_make_badarg(env);
 
   crypton_xsalsa_derive((crypton_salsa_context *) ctx, nonce.size, nonce.data);
+
   return enif_make_atom(env, "ok");
 }
 
@@ -54,7 +47,7 @@ static ERL_NIF_TERM xsalsa20_combine_nif(ErlNifEnv* env, int argc,
   void *ctx;
   ErlNifBinary src;
 
-  if (!enif_get_resource(env, argv[0], salsa_ctx, &ctx) ||
+  if (!enif_get_resource(env, argv[0], r_salsa_ctx, &ctx) ||
       !enif_inspect_binary(env, argv[1], &src))
     return enif_make_badarg(env);
 
@@ -72,7 +65,7 @@ static ERL_NIF_TERM xsalsa20_generate_nif(ErlNifEnv* env, int argc,
   void *ctx;
   uint32_t bytes;
 
-  if (!enif_get_resource(env, argv[0], salsa_ctx, &ctx) ||
+  if (!enif_get_resource(env, argv[0], r_salsa_ctx, &ctx) ||
       !enif_get_uint(env, argv[1], &bytes))
     return enif_make_badarg(env);
 
@@ -84,17 +77,62 @@ static ERL_NIF_TERM xsalsa20_generate_nif(ErlNifEnv* env, int argc,
   return ret;
 }
 
-static void salsa_ctx_dtor(ErlNifEnv* env, crypton_salsa_context *ctx) {
+static ERL_NIF_TERM poly1305_init_nif(ErlNifEnv* env, int argc,
+				      const ERL_NIF_TERM argv[])
+{
+  ErlNifBinary key;
+
+  if (!enif_inspect_binary(env, argv[0], &key) || key.size != 32)
+      return enif_make_badarg(env);
+
+  void *ctx = enif_alloc_resource(r_poly1305_ctx, sizeof(poly1305_ctx));
+  ERL_NIF_TERM ret = enif_make_resource(env, ctx);
+
+  crypton_poly1305_init((poly1305_ctx*) ctx, (poly1305_key*)key.data);
+
+  return ret;
+}
+
+static ERL_NIF_TERM poly1305_update_nif(ErlNifEnv* env, int argc,
+					const ERL_NIF_TERM argv[])
+{
+  void *ctx;
+  ErlNifBinary input;
+
+  if (!enif_get_resource(env, argv[0], r_poly1305_ctx, &ctx) ||
+      !enif_inspect_binary(env, argv[1], &input))
+    return enif_make_badarg(env);
+
+  crypton_poly1305_update((poly1305_ctx*) ctx, input.data, input.size);
+
+  return enif_make_atom(env, "ok");
+}
+
+
+static ERL_NIF_TERM poly1305_finalize_nif(ErlNifEnv* env, int argc,
+					  const ERL_NIF_TERM argv[])
+{
+  void *ctx;
+
+  if (!enif_get_resource(env, argv[0], r_poly1305_ctx, &ctx))
+    return enif_make_badarg(env);
+
+  ERL_NIF_TERM ret;
+  uint8_t* mac = enif_make_new_binary(env, 16, &ret);
+
+  crypton_poly1305_finalize(mac, (poly1305_ctx*) ctx);
+
+  return ret;
 }
 
 int on_load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
 {
   ErlNifResourceFlags tried;
-  salsa_ctx = enif_open_resource_type(env, "hsbox_nif", "xsalsa20",
-				      (ErlNifResourceDtor*) salsa_ctx_dtor,
-				      ERL_NIF_RT_CREATE, &tried);
+  r_salsa_ctx = enif_open_resource_type(env, "hsbox_nif", "xsalsa20",
+					NULL, ERL_NIF_RT_CREATE, &tried);
+  r_poly1305_ctx = enif_open_resource_type(env, "hsbox_nif", "poly1305",
+					   NULL, ERL_NIF_RT_CREATE, &tried);
   return 0;
-  
 }
 
 static ErlNifFunc nif_funcs[] = {
@@ -102,7 +140,9 @@ static ErlNifFunc nif_funcs[] = {
   {"xsalsa20_derive", 2, xsalsa20_derive_nif},
   {"xsalsa20_generate", 2, xsalsa20_generate_nif},
   {"xsalsa20_combine", 2, xsalsa20_combine_nif},
-  {"inc", 1, inc_nif}
+  {"poly1305_init", 1, poly1305_init_nif},
+  {"poly1305_update", 2, poly1305_update_nif},
+  {"poly1305_finalize", 1, poly1305_finalize_nif}
 };
 
 ERL_NIF_INIT(hsbox_nif, nif_funcs, &on_load, NULL, NULL, NULL)
--- src/hsbox_nif.erl
+++ src/hsbox_nif.erl
@@ -1,31 +1,45 @@
 -module(hsbox_nif).
 
--export([inc/1]).
 -export([xsalsa20_init/3, xsalsa20_derive/2,
 	 xsalsa20_generate/2, xsalsa20_combine/2]).
+
+-export([poly1305_init/1, poly1305_update/2, poly1305_finalize/1]).
+
 -on_load(init/0).
 
 -define(APPNAME, ?MODULE).
 -define(LIBNAME, ?APPNAME).
 
-inc(_) ->
-    erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE}).
+-type salsa20_ctx() :: reference().
+-type poly1305_ctx() :: reference().
 
 -spec xsalsa20_init(Rounds :: pos_integer(), Key :: binary(), Nonce :: binary()) ->
-	  {ok, Ctx :: _} | {error, _}.
+	  salsa20_ctx().
 xsalsa20_init(_Rounds, _Key, _Nonce) ->
     erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE}).
 
--spec xsalsa20_derive(Ctx :: _, Nonce :: binary()) -> ok | {error, _}.
+-spec xsalsa20_derive(salsa20_ctx(), Nonce :: binary()) -> ok.
 xsalsa20_derive(_Ctx, _Nonce) ->
     erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE}).
 
--spec xsalsa20_generate(Ctx :: _, Bytes :: pos_integer()) -> Dst :: binary().
+-spec xsalsa20_generate(salsa20_ctx(), NBytes :: pos_integer()) -> Dst :: binary().
 xsalsa20_generate(_Ctx, _Bytes) ->
     erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE}).
 
--spec xsalsa20_combine(Ctx :: _, Src :: binary()) -> Dst :: binary().
-xsalsa20_combine(_Ctx, _Src) ->
+-spec xsalsa20_combine(salsa20_ctx(), PT :: binary()) -> CT :: binary().
+xsalsa20_combine(_Ctx, _PT) ->
+    erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE}).
+
+-spec poly1305_init(Key :: binary()) -> poly1305_ctx().
+poly1305_init(_Key) ->
+    erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE}).
+
+-spec poly1305_update(poly1305_ctx(), Data :: binary()) -> ok.
+poly1305_update(_Ctx, _Data) ->
+    erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE}).
+
+-spec poly1305_finalize(poly1305_ctx()) -> Mac :: binary().
+poly1305_finalize(_Ctx) ->
     erlang:nif_error({nif_not_loaded, module, ?MODULE, line, ?LINE}).
 
 
